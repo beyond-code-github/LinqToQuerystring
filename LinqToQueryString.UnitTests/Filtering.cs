b6ee1f3d3d72b7f024c0539f7771c058f49b1344
Pete Smith <roysvork@gmail.com>
2013-04-01 17:48:36 +0100

    Removed some redundant code

--- LinqToQueryString.UnitTests/Filtering.cs
+++ LinqToQueryString.UnitTests/Filtering.cs
@@ -54,11 +54,11 @@
 
     public class When_using_eq_filter_on_a_single_string : Filtering
     {
-        private Because of = () => result = (IQueryable<ConcreteClass>)concreteCollection.AsQueryable().ExtendFromOData("?$filter=Name eq 'Apple'");
+        private Because of = () => result = concreteCollection.AsQueryable().ExtendFromOData("?$filter=Name eq 'Apple'");
 
         private It should_return_two_records = () => result.Count().ShouldEqual(2);
 
-        private It should_only_return_records_where_name_is_apple = () => result.Cast<ConcreteClass>().ShouldEachConformTo(o => o.Name == "Apple");
+        private It should_only_return_records_where_name_is_apple = () => result.ShouldEachConformTo(o => o.Name == "Apple");
     }
 
     public class When_using_not_eq_filter_on_a_single_string : Filtering
