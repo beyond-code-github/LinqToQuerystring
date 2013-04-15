50cc61fefadeb915afdd1e2f6a218766062d603a
Pete Smith <roysvork@gmail.com>
2013-04-15 17:36:17 +0100

    Added Nancy Demo project

---
+++ LinqToQuerystring.Demo.Nancy/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿namespace LinqToQuerystring.Demo.Nancy
+{
+    using global::Nancy;
+    using global::Nancy.Bootstrapper;
+    using global::Nancy.Diagnostics;
+    using global::Nancy.TinyIoc;
+
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override DiagnosticsConfiguration DiagnosticsConfiguration
+        {
+            get { return new DiagnosticsConfiguration { Password = @"password" }; }
+        }
+
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            StaticConfiguration.EnableRequestTracing = true;
+        }
+    }
+}
