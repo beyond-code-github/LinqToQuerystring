2131daeb752e1a1ead6007c77c22e75577772384
Pete Smith <roysvork@gmail.com>
2013-04-15 17:29:54 +0100

    Tidied up redundant code

--- LinqToQuerystring/TreeNodes/NotNode.cs
+++ LinqToQuerystring/TreeNodes/NotNode.cs
@@ -8,8 +8,6 @@
 
     using LinqToQuerystring.TreeNodes.Base;
 
-    using MongoDB.Bson;
-
     public class NotNode : SingleChildNode
     {
         public NotNode(Type inputType, IToken payload)
@@ -22,16 +20,7 @@
             var childExpression = this.ChildNode.BuildLinqExpression(query, expression, item);
             if (!typeof(bool).IsAssignableFrom(childExpression.Type))
             {
-                //if (query.Provider.GetType().Name.Contains("MongoQueryProvider"))
-                //{
-                //    //childExpression = Expression.TypeAs(childExpression, typeof(BsonBoolean));
-                //}
-                //else
-                //{
                 childExpression = Expression.Convert(childExpression, typeof(bool));
-                //}
-
-
             }
 
             return Expression.Not(childExpression);
