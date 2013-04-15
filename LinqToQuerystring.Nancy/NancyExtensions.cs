06d5bd3a3b7c1b93c74fe738c0907bee3edbdcb4
Pete Smith <roysvork@gmail.com>
2013-04-15 17:34:14 +0100

    Added LinqToQuerystring.Nancy after testing with Nancy FX

---
+++ LinqToQuerystring.Nancy/NancyExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace LinqToQuerystring.Nancy
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class NancyExtensions
+    {
+        public static dynamic LinqToQuerystring<T>(this IQueryable<T> query, IDictionary<string, object> queryDictionary, bool forceDynamicProperties = false)
+        {
+            var genericType = query.GetType().GetGenericArguments()[0];
+
+            var joined = string.Join(
+                "&", queryDictionary.Select(o => o.Key + "=" + o.Value));
+            var queryString = !string.IsNullOrEmpty(joined) ? "?" + joined : string.Empty;
+
+            return query.LinqToQuerystring(queryString, genericType, forceDynamicProperties);
+        }
+    }
+}
