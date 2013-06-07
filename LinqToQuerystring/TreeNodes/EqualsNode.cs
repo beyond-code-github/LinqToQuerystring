1607eba5d4af48a41681e668fbbe7a3aeef10a1d
Pete Smith <roysvork@gmail.com>
2013-06-07 17:34:53 +0100

    Equals node will now try to avoid comparing boolean expressions to constants true or false to sidestep linq to entities bug.

--- LinqToQuerystring/TreeNodes/EqualsNode.cs
+++ LinqToQuerystring/TreeNodes/EqualsNode.cs
@@ -20,6 +20,28 @@
             var leftExpression = this.LeftNode.BuildLinqExpression(query, expression, item);
             var rightExpression = this.RightNode.BuildLinqExpression(query, expression, item);
 
+            // Nasty workaround to avoid comparison of Aggregate functions to true or false which breaks Entity framework
+            if (leftExpression.Type == typeof(bool) && rightExpression.Type == typeof(bool) && rightExpression is ConstantExpression)
+            {
+                if ((bool)(rightExpression as ConstantExpression).Value)
+                {
+                    return leftExpression;
+                }
+
+                return Expression.Not(leftExpression);
+            }
+
+            if (rightExpression.Type == typeof(bool) && leftExpression.Type == typeof(bool)
+                && leftExpression is ConstantExpression)
+            {
+                if ((bool)(leftExpression as ConstantExpression).Value)
+                {
+                    return rightExpression;
+                }
+
+                return Expression.Not(rightExpression);
+            }
+
             if (!leftExpression.Type.IsAssignableFrom(rightExpression.Type))
             {
                 rightExpression = Expression.Convert(rightExpression, leftExpression.Type);
