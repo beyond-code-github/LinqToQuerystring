90d3d08399b8d85b990b60ae6b1118d4f6864e92
Pete Smith <roysvork@gmail.com>
2013-11-15 18:59:58 +0000

    Added LinqToQuerystring core project to WebApi solution so it can be built

--- LinqToQuerystring.WebApi/LinqToQueryableAttribute.cs
+++ LinqToQuerystring.WebApi/LinqToQueryableAttribute.cs
@@ -4,8 +4,6 @@
     using System.Net;
     using System.Net.Http;
     using System.Net.Http.Formatting;
-    using System.Web;
-    using System.Web.Http;
     using System.Web.Http.Filters;
 
     using ActionFilterAttribute = System.Web.Http.Filters.ActionFilterAttribute;
