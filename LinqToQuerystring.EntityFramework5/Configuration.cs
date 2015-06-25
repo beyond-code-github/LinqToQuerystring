d33203b38bc355abcf3989db3bbaf7c89486f14c
Gie Spaepen <giespaepen@gmail.com>
2015-06-25 15:19:49 +0200

    Splittet the EntityFramework part into two: one supporting EF5, the other EF6. Signed all the DLL's with a public key in order to used it in other contexts where signed assemblies are needed.

---
+++ LinqToQuerystring.EntityFramework5/Configuration.cs
@@ -0,0 +1,22 @@
+﻿namespace LinqToQuerystring.EntityFramework5
+{
+    using LinqToQuerystring.Utils;
+
+    public static class Configuration
+    {
+        public static void Init()
+        {
+            if (!LinqToQuerystring.Configuration.CustomNodes.ContainsKey("DbQueryProvider"))
+            {
+                LinqToQuerystring.Configuration.CustomNodes.Add("DbQueryProvider", new CustomNodeMappings());
+            }
+
+            var objectQueryNodes = LinqToQuerystring.Configuration.CustomNodes["DbQueryProvider"];
+            if (!objectQueryNodes.ContainsKey(LinqToQuerystringLexer.EXPAND))
+            {
+                objectQueryNodes.Add(
+                    LinqToQuerystringLexer.EXPAND, (type, token, factory) => new ExpandNode(type, token, factory));
+            }
+        }
+    }
+}
