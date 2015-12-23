bf3489c0ba05be273821eae2954e31cf8cde29d1
giespaepen <$Cem1apfw>
2015-12-23 15:56:16 +0100

    Changed the EntityFramework ExpandNode to support nested properties.

--- LinqToQuerystring.EntityFramework/ExpandNode.cs
+++ LinqToQuerystring.EntityFramework/ExpandNode.cs
@@ -1,20 +1,22 @@
 ﻿namespace LinqToQuerystring.EntityFramework
 {
+    using Antlr.Runtime;
+    using LinqToQuerystring.TreeNodes;
+    using LinqToQuerystring.TreeNodes.Base;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Linq;
     using System.Linq.Expressions;
 
-    using Antlr.Runtime;
-
-    using LinqToQuerystring.TreeNodes;
-    using LinqToQuerystring.TreeNodes.Base;
-
     public class ExpandNode : QueryModifier
     {
+        private List<string> members;
+
         public ExpandNode(Type inputType, IToken payload, TreeNodeFactory treeNodeFactory)
             : base(inputType, payload, treeNodeFactory)
         {
+            this.members = new List<string>();
         }
 
         public override IQueryable ModifyQuery(IQueryable query)
@@ -24,11 +26,34 @@
                 var parameter = Expression.Parameter(this.inputType, "o");
                 var childExpression = child.BuildLinqExpression(query, query.Expression, parameter);
 
-                var member = childExpression as MemberExpression;
-                query = query.Include(member.Member.Name);
+                this.VisitMember(childExpression as MemberExpression);
+
+                if (this.members.Count() > 0)
+                {
+                    query = query.Include(string.Join(".", this.members));
+                    this.members.Clear();
+                }
             }
 
             return query;
+        }
+
+        private void VisitMember(MemberExpression expression)
+        {
+            if (expression == null) return;
+
+            // Add the properties to the list
+            this.members.Add(expression.Member.Name);
+
+            // Recurse if the nested expression is a member expression
+            if (expression.Expression is System.Linq.Expressions.MemberExpression)
+            {
+                this.VisitMember((MemberExpression)expression.Expression);
+            }
+            else
+            {
+                this.members.Reverse();
+            }
         }
     }
 }
--- LinqToQuerystring/LinqToQuerystringLexer.cs
+++ LinqToQuerystring/LinqToQuerystringLexer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-// $ANTLR 3.5.0.2 C:\\Users\\zi53516\\Source\\Repos\\LinqToQuerystring\\LinqToQuerystring\\LinqToQuerystring.g 2015-06-25 14:54:11
+// $ANTLR 3.5.0.2 C:\\Users\\zi53516\\Source\\Repos\\LinqToQuerystring\\LinqToQuerystring\\LinqToQuerystring.g 2015-12-23 15:38:17
 
 // The variable 'variable' is assigned but its value is never used.
 #pragma warning disable 219
